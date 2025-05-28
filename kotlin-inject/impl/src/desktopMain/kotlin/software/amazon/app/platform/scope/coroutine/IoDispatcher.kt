12aed0873f7e2e489da4b40219652ad01ab87e55
Ralf Wondratschek <rwo@amazon.com>
2025-05-28 00:19:20 -0700

    WASM (5)
    Clean up some duplication after the `noWasmJs` source set was introduced.

--- kotlin-inject/impl/src/desktopMain/kotlin/software/amazon/app/platform/scope/coroutine/IoDispatcher.kt
+++
@@ -1,7 +0,0 @@
-package software.amazon.app.platform.scope.coroutine
-
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-
-/** Expect declaration for the IO dispatcher, because it doesn't exist for WASM. */
-internal actual val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
